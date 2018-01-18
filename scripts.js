54ab852f389928dd70793d8b496162af41a153b9
Matthew Aderhold <matthewaderhold@gmail.com>
2018-01-18 10:02:27 -0600

    Initial framework

---
+++ scripts.js
@@ -0,0 +1,53 @@
+class Shape {
+  construstor() {
+    //Parent Class Properties
+  }
+  //Parent Class Methods
+  method() {
+    //method actions
+  }
+}
+
+class Circle extends Shape {
+  constructor() {
+    //Inherited Properties go in Super(here, here)
+    super();
+    //class specific properties go here
+  }
+  classSpecificMethod() {
+    //class specific method
+  }
+}
+
+class Triangle extends Shape {
+  constructor() {
+    //Inherited Properties go in Super(here, here)
+    super();
+    //class specific properties go here
+}
+  classSpecificMethod() {
+    //class specific method
+  }
+}
+
+class Rectangle extends Shape {
+  constructor() {
+    //Inherited Properties go in Super(here, here)
+    super();
+    //class specific properties go here
+  }
+  classSpecificMethod() {
+    //class specific method
+  }
+}
+
+class Square extends Shape {
+  constructor() {
+    //Inherited Properties go in Super(here, here)
+    super();
+    //class specific properties go here
+}
+  classSpecificMethod() {
+    //class specific method
+  }
+}
